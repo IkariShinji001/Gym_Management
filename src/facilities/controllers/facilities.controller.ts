5ac6900fdbd03914e2adc65e92821e7e615c765a
Thanh-Thong-03 <mathanhthong462@gmail.com>
2024-08-15 20:01:26 +0700

    facility

--- src/facilities/controllers/facilities.controller.ts
+++ src/facilities/controllers/facilities.controller.ts
@@ -1,13 +1,19 @@
 import { Facilities } from '../repositories/facilities.entity';
 import { FacilitiesService } from '../services/facilities.service';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('/facilities')
 export class FacilitiesController {
   constructor(private facilitiesService: FacilitiesService) {}
 
   @Get()
-  getFacilities(): Promise<Facilities[]> {
-    return this.facilitiesService.findAll();
+  async findAll(): Promise<Facilities[]> {
+    return await this.facilitiesService.findAll();
   }
+  @Post()
+  async Create(): Promise<Facilities> {
+    return await this.facilitiesService.create();
+  }
+  
+
 }
--- src/facilities/services/facilities.service.ts
+++ src/facilities/services/facilities.service.ts
@@ -2,14 +2,31 @@ import { Injectable } from '@nestjs/common';
 import { Facilities } from '../repositories/facilities.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { IFacilitiesService } from '../interfaces/facilities.service.interface';
+import { CreateFacilityDto, updateFacilityDto } from '../dtos/facilities.dto';
 
 @Injectable()
-export class FacilitiesService {
+export class FacilitiesService implements IFacilitiesService{
   constructor(
     @InjectRepository(Facilities)
     private facilitiesRepository: Repository<Facilities>,
   ) {}
-
+  async findOne(id: number): Promise<Facilities> {
+    return await this.facilitiesRepository.findOne({where: {id}});
+  }
+  async create(newFacility: CreateFacilityDto): Promise<Facilities> {
+    const facility = await this.facilitiesRepository.create(newFacility);
+    return await this.facilitiesRepository.save(facility);
+  }
+  update(id: number, updateFacility: updateFacilityDto): Promise<Facilities> {
+    throw new Error('Method not implemented.');
+  }
+  delete(id: number): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  findByName(name: string): Promise<Facilities[]> {
+    throw new Error('Method not implemented.');
+  }
   async findAll() {
     return await this.facilitiesRepository.find();
   }
