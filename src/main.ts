cf114cc846ebc4208d60a1483ab7ba81b9b0a700
IkariShinji001 <duytran2003dev@gmail.com>
2024-08-16 10:34:27 +0700

    admin

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './config/http-exception.filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(cookieParser());
   await app.listen(3001);
 }
 bootstrap();
