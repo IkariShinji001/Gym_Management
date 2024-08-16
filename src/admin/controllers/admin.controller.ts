34997176912e74ac1bbe750efd94f319b1d4eb3c
chicken lee <tuankie191103@gmail.com>
2024-08-16 10:52:20 +0700

    admin controller, service

--- src/admin/controllers/admin.controller.ts
+++ src/admin/controllers/admin.controller.ts
@@ -1,6 +1,35 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Admin } from "../repositories/admin.entity";
+import { AdminService } from "../services/admin.services";
+import { CreateAdminDto, updateAdminDto } from "../dtos/admin.dto";
 
-@Controller('admin')
+@Controller('/admin')
 export class AdminController {
-  constructor() {}
+  constructor( private adminService: AdminService) {}
+
+  @Get()
+  async getAdmins(): Promise<Admin[]> {
+    return await this.adminService.findAll();
+  }
+  @Get()
+  async findOneAdmin(@Param('id') id: number): Promise<Admin> {
+    return await this.adminService.findOne(id);
+  }
+  @Post()
+  async createAdmin(@Body() CreateAdminDto: CreateAdminDto): Promise<Admin> {
+    return await this.adminService.create(CreateAdminDto);
+  }
+  @Patch(':id')
+  async updateAdmin(@Param('id') id: number, @Body() updateAdminDto: updateAdminDto){
+    return await this.adminService.update(id, updateAdminDto)
+  }
+  @Delete(':id')
+  async deleteAdmin(@Param('id') id:number){
+    return await this.adminService.delete(id);
+  }
+  @Get('phone/:phoneNumber')
+  async findByPhoneNumber(@Param('phoneNumber') phoneNumber: string): Promise<Admin[]> {
+    return await this.adminService.findByPhoneNumber( phoneNumber);
+  }
+
 }
--- src/admin/services/admin.services.ts
+++ src/admin/services/admin.services.ts
@@ -1,33 +1,40 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { IAdminService } from "../interfaces/admin.service.interface";
-import { CreateAdminDto, updateAdminDto } from "../dtos/admin.dto";
-import { Admin } from "../repositories/admin.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Like, Repository } from 'typeorm';
+import { IAdminService } from '../interfaces/admin.service.interface';
+import { CreateAdminDto, updateAdminDto } from '../dtos/admin.dto';
+import { Admin } from '../repositories/admin.entity';
 
 @Injectable()
-export class AdminService implements IAdminService{
+export class AdminService implements IAdminService {
   constructor(
     @InjectRepository(Admin)
     private adminRepository: Repository<Admin>,
   ) {}
-    async findOne(id: number): Promise<Admin> {
-        return await this.adminRepository.findOne({ where: { id } });
-    }
-    async create(newAdmin: CreateAdminDto): Promise<Admin> {
-        const createdAdmin = this.adminRepository.create(newAdmin);
-        return await this.adminRepository.save(createdAdmin);
-    }
-    update(id: number, updateAdmin: updateAdminDto): Promise<Admin> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: number): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    findByPhoneNumber(phoneNumber: string): Promise<Admin[]> {
-        throw new Error("Method not implemented.");
-    }
-    async findAll() {
-        return await this.adminRepository.find();
-    }
+  async findAll(): Promise<Admin[]> {
+    return await this.adminRepository.find();
+  }
+  async findOne(id: number): Promise<Admin> {
+    return await this.adminRepository.findOne({ where: { id } });
+  }
+  async create(newAdmin: CreateAdminDto): Promise<Admin> {
+    const createdAdmin = this.adminRepository.create(newAdmin);
+    return await this.adminRepository.save(createdAdmin);
+  }
+  async update(id: number, updateAdmin: updateAdminDto): Promise<Admin> {
+    await this.adminRepository.update(id, updateAdmin);
+    return this.adminRepository.findOne({ where: { id } });
+  }
+  async delete(id: number): Promise<void> {
+    const deletedAdmin = await this.adminRepository.findOne({ where: { id } });
+    await this.adminRepository.delete(deletedAdmin);
+  }
+  async findByPhoneNumber(phoneNumber: string): Promise<Admin[]> {
+    const admins = await this.adminRepository.find({
+      where: {
+        phoneNumber: Like(`%${phoneNumber}%`),
+      },
+    });
+    return admins;
+  }
 }
